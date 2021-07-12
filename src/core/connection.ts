ae095e247178f355efbfd0cc597893fe07666dcf
nakulnambiar <nakul.nambiar@demandfarm.com>
2021-07-12 21:12:07 +0530

    Minor configuration changes

--- src/core/connection.ts
+++ src/core/connection.ts
@@ -3,7 +3,9 @@ const env = process.env.NODE_ENV || 'development';
 // const config = require(__dirname + '/../config/config.json')[env];
 import envConfig from "../config/config.json";
 
-let config: any = envConfig.development;
+let config: any = null;
+if(env == "production") config = envConfig.production;
+else config = envConfig.development;
 let sequelize: Sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 export {sequelize};
