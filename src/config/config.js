862ef701edadeae1701d915c27e6b2f5d9ac5f06
nakulnambiar <nakul.nambiar@demandfarm.com>
2021-07-13 00:18:32 +0530

    Workaround

--- src/config/config.js
+++ src/config/config.js
@@ -2,10 +2,12 @@ const fs = require('fs');
 
 module.exports = {
     development: {
-        username: "root",
-        password: "",
-        database: "portfolio_tracker",
-        host: "127.0.0.1",
+        // username: "root",
+        // password: "",
+        // database: "portfolio_tracker",
+        // host: "127.0.0.1",
+        // dialect: "mysql"
+        connectString: process.env.CLEARDB_DATABASE_URL,
         dialect: "mysql"
     }, 
     production: {
