e50826e451b7817bf74b2f9e9f3e4b5cc67864b9
nakulnambiar <nakul.nambiar@demandfarm.com>
2021-07-13 01:19:49 +0530

    Minor change

--- src/core/connection.ts
+++ src/core/connection.ts
@@ -4,10 +4,10 @@ import sequelizeConfig from "../config/config.json"
 let sequelize: Sequelize;
 
 if(env === "production") {
-    let connectString: any =  sequelizeConfig.production.use_env_variable;
+    let connectString: any =  sequelizeConfig.production.use_env_variable || process.env.CLEARDB_DATABASE_URL;
     sequelize = new Sequelize(connectString, {dialect: "mysql"});
 } else {
-    let config: any =  sequelizeConfig.development;
+    let config: any =  sequelizeConfig.development || process.env.CLEARDB_DATABASE_URL;
     sequelize = new Sequelize(config.database, config.username, config.password, config);
 } 
 
