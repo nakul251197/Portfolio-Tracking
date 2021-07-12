b08c18041f13b24d369b062d29af21d980c6193e
nakulnambiar <nakul.nambiar@demandfarm.com>
2021-07-12 23:43:04 +0530

    Bug fix

--- src/core/connection.ts
+++ src/core/connection.ts
@@ -1,7 +1,8 @@
 import { Sequelize } from "sequelize";
 const env = process.env.NODE_ENV || 'development';
-const sequelizeConfig = require('config/config');
+const sequelizeConfig = require('../config/config');
 let sequelize: Sequelize;
+
 if(env == "production") {
     let connectString: any =  sequelizeConfig.production.connectString;
     sequelize = new Sequelize(connectString);
